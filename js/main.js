770b82295983b79ad447e3dbf4d65b53422dac9e
Henry Tian <henry@maneless.com>
2013-03-01 01:34:43 -0500

    Replaced links in navigation bar with scrolling effects

--- js/main.js
+++ js/main.js
@@ -2,16 +2,38 @@
 
 var TAN = {};
 
-$(document).ready(function() {
-    TAN.menuExpanded = false;
+  $(document).ready(function() {
+  TAN.menuExpanded = false;
 
-    $('#menu-button').click(function(event) {
-        if (TAN.menuExpanded) {
-            $('#menu').hide();
-            TAN.menuExpanded = false;
-        } else {
-            $('#menu').show();
-            TAN.menuExpanded = true;
-        }
-    });
+  $('#menu-button').click(function(event) {
+    if (TAN.menuExpanded) {
+      $('#menu').hide();
+      TAN.menuExpanded = false;
+    } else {
+      $('#menu').show();
+      TAN.menuExpanded = true;
+    }
+  });
+
+  $('.nav-action').click(function(event) {
+    event.preventDefault();
+
+    var selected_id = $(this).attr('id');
+    selected_id = selected_id.substring(0, selected_id.length - 4); // Length of '_nav' is 4
+
+    // Height of title bar is 36px
+    var scrollPosition = $('#' + selected_id).position().top;
+    if (TAN.menuExpanded) {
+      scrollPosition -= 36;
+    }
+
+    $('html, body').animate({
+      scrollTop: scrollPosition
+    }, 'slow');
+
+    if (TAN.menuExpanded) {
+      $('#menu').hide();
+      TAN.menuExpanded = false;
+    }
+  });
 });
