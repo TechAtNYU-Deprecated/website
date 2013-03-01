9030a376b893198a60907af229429785f416506f
Henry Tian <henry@maneless.com>
2013-03-01 02:14:48 -0500

    Made main.js more readable

--- js/main.js
+++ js/main.js
@@ -1,40 +1,46 @@
 'use strict';
 
-var TAN = {};
-
-  $(document).ready(function() {
-  TAN.menuExpanded = false;
-
-  $('#menu-button').click(function(event) {
+var TAN = {
+  init: function() {
+    this.menuExpanded = false;
+    this.getSelectors();
+    this.setEvents();
+  },
+  getSelectors: function () {
+    this.$menu = $('#menu');
+    this.$menuButton = $('#menu-button');
+    this.$navActions = $('.nav-action');
+  },
+  setEvents: function (){
+    this.$menuButton.click(this.toggleMenu);
+    this.$navActions.click(this.scrollNav);
+  },
+  scrollNav: function(event) {
     event.preventDefault();
-
-    if (TAN.menuExpanded) {
-      $('#menu').hide();
-      TAN.menuExpanded = false;
-    } else {
-      $('#menu').show();
-      TAN.menuExpanded = true;
-    }
-  });
-
-  $('.nav-action').click(function(event) {
-    event.preventDefault();
-
     var selected_id = $(this).attr('id');
     selected_id = selected_id.substring(0, selected_id.length - 4); // Length of '_nav' is 4
-
     var scrollPosition = $('#' + selected_id).position().top;
     if (TAN.menuExpanded) {
       scrollPosition -= 36; // Height of title bar is 36px
     }
-
     $('html, body').animate({
       scrollTop: scrollPosition
     }, 'slow');
-
     if (TAN.menuExpanded) {
-      $('#menu').hide();
+      TAN.$menu.hide();
       TAN.menuExpanded = false;
     }
-  });
-});
+  },
+  toggleMenu: function(event) {
+    event.preventDefault();
+    if (TAN.menuExpanded) {
+      TAN.$menu.hide();
+      TAN.menuExpanded = false;
+    } else {
+      TAN.$menu.show();
+      TAN.menuExpanded = true;
+    }
+  }
+};
+
+TAN.init();
