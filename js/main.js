a3dc49a11c0c5928874975f613f11ccfb34f5ae6
Henry Tian <henry@maneless.com>
2013-03-01 00:53:58 -0500

    Added title bar and collapsed navigation bar into menu button for mobile devices (<35em)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+'use strict';
 
+var TAN = {};
+
+$(document).ready(function() {
+    TAN.menuExpanded = false;
+
+    $('#menu-button').click(function(event) {
+        if (TAN.menuExpanded) {
+            $('#menu').hide();
+            TAN.menuExpanded = false;
+        } else {
+            $('#menu').show();
+            TAN.menuExpanded = true;
+        }
+    });
+});
